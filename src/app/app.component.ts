ff8b0281cbb8dbb5fd4d537afa55e2f55886a0b9
Cristian Hourcade <chourcade@baufest.com>
2019-10-07 09:10:38 -0300

    adapting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,7 @@ export class AppComponent implements OnInit{
 
 
   listado = [{status:"red",nombre:"Toto",unidad:"UX",rol:"Analista funcional",horas:"120hs",Evaluador:"Erika, la torre"},
-  {status:"yellow",nombre:"Toto",unidad:"UX",rol:"Analista funcional",horas:"120hs",Evaluador:"Erika, la torre"},
+  {status:"#000",nombre:"Toto",unidad:"UX",rol:"Analista funcional",horas:"120hs",Evaluador:"Erika, la torre"},
   {status:"yellow",nombre:"Toto",unidad:"UX",rol:"Analista funcional",horas:"120hs",Evaluador:"Erika, la torre"},
   {status:"yellow",nombre:"Toto",unidad:"UX",rol:"Analista funcional",horas:"120hs",Evaluador:"Erika, la torre"},
   {status:"green",nombre:"Toto",unidad:"UX",rol:"Analista funcional",horas:"120hs",Evaluador:"Erika, la torre"}]
